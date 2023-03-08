6f198db6e458437a3342a11f071e70101fb49a40
Ralph Cox <73188137+essexgit@users.noreply.github.com>
2023-03-08 00:12:47 +0000

    final prior to submission

--- reactportfolio/src/components/pages/Experience.js
+++ reactportfolio/src/components/pages/Experience.js
@@ -4,16 +4,181 @@ import "./pages.css";
 function Experience() {
 
   return (
-    <div>
-      <h1>Experience Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <main>
+      <section class="segment provocative">
+        <div class="titleWrapper">
+          <h3>Provocative</h3>
+        </div>
+        <p>My boss had a problem. Housing policy leads were blocking ideas to build more homes. He was meeting them
+          tomorrow; could
+          I help? Next day, my presentation turned the situation around; I’m a <strong class="shimmer openStrategist"
+            tabindex="0" role="dialog" aria-label="my definition of strategist">Strategist</strong>.
+        </p>
+        <p>I grasp the root cause of a
+          problem,
+          embrace uncertainty, and compare solutions. I need to understand holistically and flip between viewpoints.
+          I
+          overcome
+          entrenched beliefs, posing problematic questions.
+        </p>
+      </section>
+      <section class="segment perceptive">
+        <div class="titleWrapper">
+          <h3>Perceptive</h3>
+        </div>
+        <p><strong class="shimmer openStealth" tabindex="0" role="dialog" aria-label="about stealth dyslexia">Stealth
+          Dyslexia</strong> defined school and
+          career.
+          Computers
+          enabled me to write. I won ‘best new play’ in
+          the
+          Southend
+          Drama festival and achieved three readings at the National Theatre. My screenplays attracted consideration
+          from
+          producers,
+          directors and performers including Jack Shepherd, and Patrick Allen.</p>
+        <p> I moved to London to train as an
+          actor, with a
+          talent for improvisation and directing; I performed in Fringe Theatre, short films, and TV.</p>
+      </section>
+      <section class="segment perspective">
+        <div class="titleWrapper">
+          <h3>Perspective</h3>
+        </div>
+        <p>Acting & writing taught me psychology, motivations, biases, beliefs, incentives, and interactions. I
+          plunge
+          into
+          research for every new encounter.</p>
+        <p> When diagnosed Dyslexic at HMT, I
+          accelerated my reading, digesting over
+          400
+          audio-books: Strategy, Economics, Philosophy, Psychology, Business, Sales, Communication, Marketing,
+          Statistics, Science…no limits.</p>
+        <p> Technology increased my reading and writing speed, and I developed expertise
+          in critical
+          thinking and problem solving, by relentless, eclectic learning.
+        </p>
+      </section>
+      <section class="segment proven">
+        <div class="career">
+          <div class="titleWrapper">
+            <h3>Proven</h3>
+          </div>
+          <div class="dates activities current">
+            <div class="date">
+              <h4>April 2018
+                <strong>Present</strong>
+              </h4>
+            </div>
+            <div class="place">
+              <h4>Freelance Strategy Consultant</h4>
+            </div>
+            <div class="activities">
+              <p>
+                Providing insights on UK House Building Market and Government policies and
+                processes, for overseas investors and developers.
+              </p>
+            </div>
+          </div>
+          <div class="dates activities dclg">
+            <div class="date">
+              <h4>January 2013 - March
+                2018</h4>
+            </div>
+            <div class="place">
+              <h4>Department for Communities and Local Government, Strategic Advisor</h4>
+            </div>
+            <div class="activities">
+              <p>
+                I changed government policy on accelerating planning permissions by presenting
+                evidence exposing misconceptions and
+                reverse engineering the business models of major builders.</p>
+              <p>
+                I chaired the Disability Network, leading members to a strategic focus on Mental Health, driving over
+                300
+                responses to
+                internal disability attitudes survey, and using the results to alter operational policy.
+              </p>
+            </div>
+          </div>
+          <div class="dates activities voa">
+            <div class="date">
+              <h4>February 2012 -
+                December 2012</h4>
+            </div>
+            <div class="place">
+              <h4>Valuation Office Agency, Programme & Project Coordinator.</h4>
+            </div>
+            <div class="activities">
+              <p>I facilitated a mediation session between project teams in conflict over a cross-cutting resource and
+                scheduling dispute
+                and negotiated an agreement by drawing out underlying interests and concerns.</p>
+            </div>
+          </div>
+          <div class="dates activities hmrc">
+            <div class="date">
+              <h4>January 2009  - February
+                2012</h4>
+            </div>
+            <div class="place">
+              <h4>HM Revenue & Customs, Knowledge Analysis & Intelligence - Change Programme Officer.</h4>
+            </div>
+            <div class="activities">
+              <p>I designed Project and Programme Management procedures, guidance, and templates. Designing the
+                Knowledge Management
+                improvement programme. Simplifying Nonaka & Takeuchi SECI Model with Lean, System Dynamics and
+                Behavioural Insights.
+              </p>
+            </div>
+          </div>
+          <div class="dates activities hmt">
+            <div class="date">
+              <h4>March 2007 - January
+                2009</h4>
+            </div>
+            <div class="place">
+              <h4 id="hmt">HM Treasury - Pensions Equality & Disability Team - Strategic Policy
+                Analyst.</h4>
+            </div>
+            <div class="activities">
+              <p>
+                I developed the evidence base for pension reform, incorporating behavioural
+                economics of choice architecture and default
+                design, which underpinned pension auto-enrolment policy.</p>
+              <p> I advised Ministers and officials in DWP on
+                optimising default
+                pension scheme design.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="segment persistent">
+        <div class="titleWrapper">
+          <h3>Persistent</h3>
+        </div>
+        <p>
+          Manufacturing production control (8 years), telesales (1 year) and direct charity
+          fundraising (6
+          years); MK Electric (responsible for £13m output) progressing supply, planning and scheduling factory
+          production,
+          Yardley coordinating sub-contract organising production scale product trials.</p>
+        <p>I carried out telephone market
+          research
+          interviews for a research agency, making B2B, B2C research and political opinion surveys. I cold called,
+          selling
+          advertising space in directories and member magazines.</p>
+        <p>
+          I called contacts of charities for donations, objection handling, negotiating, and obtaining personal
+          information. Top
+          fundraiser for Sudan Campaign, averaging £1,000ph.
+        </p>
+
+      </section>
+      <footer>
+        © Ralph Cox, design and build, March 2023.
+      </footer>
+    </main>
   );
 }
 
--- reactportfolio/src/components/pages/Home.js
+++ reactportfolio/src/components/pages/Home.js
@@ -5,12 +5,16 @@ import SvgPortrait from "../ImportImages/SVGPortrait";
 
 function Home(props) {
   return (
-    <div className="home--layout">
-      <div className="page--title">
+    <div className="container">
+      <div className="pageTitle">
         <SvgTitle />
       </div>
-      <div className="page--portrait">
+      <div className="pagePortrait">
         <SvgPortrait />
+      </div>
+      <div class="sloganWrapper">
+        <h1 className="sloganA">Make the Rules</h1>
+        <h1 className="sloganB">Shout the Loudest</h1>
       </div>
     </div>
   );
